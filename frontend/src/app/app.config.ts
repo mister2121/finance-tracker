628c7658cccd4deae6726dde65f33ecb16e1a215
mister2121 <mrudzki22@gmail.com>
2026-06-22 00:40:08 +0200

    add transactions page ui (frontend)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
+
+registerLocaleData(localePl);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([authInterceptor])),
+    { provide: LOCALE_ID, useValue: 'pl-PL' },
+  ],
 };
--- frontend/src/app/features/transactions/transactions.component.ts
+++ frontend/src/app/features/transactions/transactions.component.ts
@@ -1,7 +1,12 @@
 import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
-import { TransactionService } from '../../core/services/transaction.service';
-import { PagedResponse, TransactionResponse } from '../../core/models/transaction.model';
 import { CommonModule } from '@angular/common';
+import { TransactionService } from '../../core/services/transaction.service';
+import {
+  PagedResponse,
+  TransactionResponse,
+  TransactionType,
+} from '../../core/models/transaction.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-transactions',
@@ -11,26 +16,67 @@ import { CommonModule } from '@angular/common';
 export class TransactionsComponent implements OnInit {
   private transactionService = inject(TransactionService);
   private cdr = inject(ChangeDetectorRef);
+  private currentRequest?: Subscription;
 
   transactions: PagedResponse<TransactionResponse> | null = null;
-  currentYear: string = new Date().toLocaleString('pl-PL', { year: 'numeric' });
-  currentMonth: string = new Date().toLocaleString('pl-PL', { month: 'long' });
+  selectedDate = new Date();
 
   ngOnInit() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth() + 1;
+    this.loadTransactions();
+  }
+
+  loadTransactions() {
+    this.currentRequest?.unsubscribe();
+
+    const year = this.selectedDate.getFullYear();
+    const month = this.selectedDate.getMonth() + 1;
     const page = 0;
     const size = 20;
 
     this.transactionService.getTransactions(year, month, page, size).subscribe({
       next: (transactions) => {
         this.transactions = transactions;
-        this.cdr.detectChanges(); // i dont know why but this is necessary
+        this.cdr.detectChanges();
       },
       error: () => {
         alert('Failed to load transactions data. Please try again later.');
       },
     });
+  }
+
+  previousMonth() {
+    this.selectedDate = new Date(
+      this.selectedDate.getFullYear(),
+      this.selectedDate.getMonth() - 1,
+      1,
+    );
+    this.loadTransactions();
+  }
+
+  nextMonth() {
+    this.selectedDate = new Date(
+      this.selectedDate.getFullYear(),
+      this.selectedDate.getMonth() + 1,
+      1,
+    );
+    this.loadTransactions();
+  }
+
+  get selectedMonthLabel(): string {
+    const label = this.selectedDate.toLocaleString('pl-PL', { month: 'long', year: 'numeric' });
+    return label.charAt(0).toUpperCase() + label.slice(1);
+  }
+
+  getAmountPrefix(type: TransactionType): string {
+    if (type === TransactionType.INCOME) return '+';
+    if (type === TransactionType.EXPENSE) return '-';
+    return '';
+  }
+
+  setColorBasedOnType(type: TransactionType): string {
+    if (type === TransactionType.INCOME) return 'text-money-green';
+    if (type === TransactionType.EXPENSE) return 'text-money-red';
+    if (type === TransactionType.TRANSFER) return 'text-money-blue';
+    return 'text-white';
   }
 }
