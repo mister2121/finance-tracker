77667b08bf2d8582f3669ac04dd37ea45b463ace
mister2121 <mrudzki22@gmail.com>
2026-07-14 14:30:33 +0200

    add: validation for categories page

--- frontend/src/app/features/categories/add-category-modal/add-category-modal.ts
+++ frontend/src/app/features/categories/add-category-modal/add-category-modal.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, Input, OnInit } from '@angular/core';
 import { CategoryService } from '../../../core/services/category.service';
 import { ModalService } from '../../../core/services/modal.service';
 import { Category, CategoryRequest, CategoryType } from '../../../core/models/category.model';
@@ -13,12 +13,35 @@ import { FormsModule } from '@angular/forms';
 export class AddCategoryModal implements OnInit {
   private categoryService = inject(CategoryService);
   modalService = inject(ModalService);
+  private cdr = inject(ChangeDetectorRef);
 
   CategoryType = CategoryType;
   name = '';
   selectedType: CategoryType | null = null;
 
+  // błędy:
+  nameError = '';
+  selectedTypeError = '';
+
   @Input() categoryToEdit: Category | null = null;
+
+  validateCategoryForm(): boolean {
+    this.nameError = '';
+    this.selectedTypeError = '';
+    let isValid = true;
+
+    if (!this.name.trim()) {
+      this.nameError = 'Nazwa jest wymagana.';
+      isValid = false;
+    }
+
+    if (this.selectedType == null) {
+      this.selectedTypeError = 'Musisz wybrać kategorię.';
+      isValid = false;
+    }
+
+    return isValid;
+  }
 
   ngOnInit() {
     if (this.categoryToEdit) {
@@ -28,6 +51,13 @@ export class AddCategoryModal implements OnInit {
   }
 
   onSubmit() {
+    const formValid = this.validateCategoryForm();
+
+    if (!formValid) {
+      this.cdr.detectChanges();
+      return;
+    }
+
     if (!this.selectedType) {
       return;
     }
